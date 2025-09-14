ea69e85f3b4261f83d1d00a35fffb900e9b75333
mateus <mateusmacedoeng@gmail.com>
2025-09-14 18:14:26 -0300

    restart project

--- src/toggleButton.js
+++
@@ -1,8 +0,0 @@
-const toggleSwitch = document.getElementById("toggleSwitch");
-const labelStatus = document.getElementById("status");
-
-toggleSwitch.addEventListener("change", () => {
-    labelStatus.textContent = toggleSwitch.checked
-        ? "Não repetir número"
-        : "Repetir número";
-});
