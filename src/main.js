a47bc0bb53e8055e98620f53a03bf816c3b3f85f
Mateus <mateusmacedoeng@gmail.com>
2025-10-18 18:51:01 -0300

    remaking the project

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import {handleSwitchButton} from "./utils/switchButton.js"
-
-const quantNumbers = document.getElementById('quant-numbers')
-const startingNumber = document.getElementById('starting-number')
-const finalNumber = document.getElementById('final-number')
-
-handleSwitchButton()
