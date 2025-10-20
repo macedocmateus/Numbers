e80c100484a6417fae64a849608df886d178d0c4
Mateus <mateusmacedoeng@gmail.com>
2025-10-20 17:23:43 -0300

    fix hovers in inputs and submit button

--- main.js
+++ main.js
@@ -15,6 +15,30 @@ function handleSwitchButton() {
     switchButton.addEventListener('change', updateLabel);
 }
 
-handleSwitchButton();
+function handleInputGradient() {
+    const inputs = document.querySelectorAll('.main-content .form-item input');
 
-export { handleSwitchButton }
+    inputs.forEach(input => {
+        input.addEventListener('input', (event) => {
+            if (event.target.value.trim() !== '') {
+                event.target.classList.add('has-value');
+            } else {
+                event.target.classList.remove('has-value');
+            }
+        });
+    });
+}
+
+function preventFormSubmit() {
+    const form = document.querySelector('.main-content form');
+    
+    if (form) {
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+        });
+    }
+}
+
+preventFormSubmit();
+handleSwitchButton();
+handleInputGradient();
